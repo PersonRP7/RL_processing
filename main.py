1862bea4c37c9bc12fb1b3a3b557a5e987c419d6
me <avos.mail.dev@gmail.com>
2025-09-14 10:02:56 +0200

    add_json_sorting feat: Formatting
    Rm unused code from `main.py`.

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 from fastapi import FastAPI, Request
 import tempfile
 from fastapi.responses import JSONResponse, StreamingResponse
-# from services.name_processing import convert_to_ndjson
 from services.name_processing import convert_to_ndjson_stream
 
 app = FastAPI()
 
-# @app.post("/upload-json")
-# async def upload_json(request: Request):
-#     # Step 1: Save uploaded JSON to a temporary file in chunks
-#     temp = tempfile.NamedTemporaryFile(delete=False, suffix=".json")
-
-#     async for chunk in request.stream():
-#         temp.write(chunk)
-
-#     temp.close()
-#     convert_to_ndjson(temp.name)
-#     return JSONResponse({"message": "Upload successful", "temp_file": temp.name})
 
 @app.post("/upload-json-stream")
 async def upload_json_stream(request: Request):
